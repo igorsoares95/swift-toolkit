0a9e4abf9e664dd478fd3eb9a3f8c664575025c2
Igor Soares <39347752+igorsoares95@users.noreply.github.com>
2022-03-31 16:04:30 -0300

    fix: set exact swiftsoup version

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/marmelroy/Zip.git", from: "2.1.1"),
         .package(url: "https://github.com/ra1028/DifferenceKit.git", from: "1.2.0"),
         .package(url: "https://github.com/readium/GCDWebServer.git", from: "3.7.0"),
-        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.3.2"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", .exact("2.3.8")),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.2"),
         // 0.9.12 requires iOS 12+
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", "0.9.0"..<"0.9.12"),
